5e9e4f25dcce6b7e2f25898ca925664d5ef1e402
Jaehyeon Kim <dottami@gmail.com>
2022-07-28 08:18:10 +1000

    update lambda function

--- lambda/src/lambda_function.py
+++ lambda/src/lambda_function.py
@@ -1,23 +1,36 @@
 import time
+import functools
 from aws_lambda_powertools import Logger
 from aws_lambda_powertools.utilities.typing import LambdaContext
 
 logger = Logger()
 
 
-@logger.inject_lambda_context
+def on_function_ended(func):
+    @functools.wraps(func)
+    def wrapper(*args, **kwargs):
+        try:
+            value = func(*args, **kwargs)
+            logger.info("Function ended")
+            return value
+        except Exception:
+            logger.error("Function ended")
+
+    return wrapper
+
+
+@logger.inject_lambda_context(correlation_id_path="correlation_id")
+@on_function_ended
 def lambda_handler(event: dict, context: LambdaContext):
-    correlation_id = event["correlation_id"]
     num_iter = event.get("n", 10)
     to_fail = event.get("to_fail", False)
-    logger.set_correlation_id(correlation_id)
     logger.info(f"num_iter - {num_iter}, fail - {to_fail}")
     try:
         for n in range(num_iter):
             logger.info(f"iter - {n + 1}...")
-            if to_fail and (n + 1) == num_iter:
-                raise Exception
             time.sleep(1)
+        if to_fail:
+            raise Exception
     except Exception as e:
         logger.exception("Function invocation failed...")
         raise RuntimeError("Unable to finish loop") from e
