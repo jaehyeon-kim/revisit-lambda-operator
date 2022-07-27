3b38388d7e937486977d73d2c993ef935dac0002
Jaehyeon Kim <dottami@gmail.com>
2022-07-28 09:45:39 +1000

    simplify decorator

--- lambda/src/lambda_function.py
+++ lambda/src/lambda_function.py
@@ -2,25 +2,21 @@ import time
 import functools
 from aws_lambda_powertools import Logger
 from aws_lambda_powertools.utilities.typing import LambdaContext
+from aws_lambda_powertools.middleware_factory import lambda_handler_decorator
 
 logger = Logger()
 
 
-def on_function_ended(func):
-    @functools.wraps(func)
-    def wrapper(*args, **kwargs):
-        try:
-            value = func(*args, **kwargs)
-            logger.info("Function ended")
-            return value
-        except Exception:
-            logger.error("Function ended")
-
-    return wrapper
+@lambda_handler_decorator
+def middleware_before_after(handler, event, context):
+    logger.info("Function started")
+    response = handler(event, context)
+    logger.info("Function ended")
+    return response
 
 
 @logger.inject_lambda_context(correlation_id_path="correlation_id")
-@on_function_ended
+@middleware_before_after
 def lambda_handler(event: dict, context: LambdaContext):
     num_iter = event.get("n", 10)
     to_fail = event.get("to_fail", False)
