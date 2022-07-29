7067efd7547d12b2c6846dead9dd421fbe669f3d
Jaehyeon Kim <dottami@gmail.com>
2022-07-30 07:34:59 +1000

    add example_with_logging dag

--- airflow/dags/example_dags.py
+++ airflow/dags/example_dags.py
@@ -1,26 +1,26 @@
 import os
 import json
 from datetime import datetime, timedelta
-from uuid import uuid4
+
 from airflow import DAG
 from airflow.providers.amazon.aws.operators.aws_lambda import AwsLambdaInvokeFunctionOperator
-
+from lambda_operator import CustomLambdaFunctionOperator
 
 LAMBDA_FUNCTION_NAME = os.getenv("LAMBDA_FUNCTION_NAME", "example-lambda-function")
 
 
-def _set_payload(n: int = 10, to_fail: bool = True, correlation_id: str = str(uuid4())):
-    return json.dumps({"n": n, "to_fail": to_fail, "correlation_id": correlation_id})
+def _set_payload(n: int = 10, to_fail: bool = True):
+    return json.dumps({"n": n, "to_fail": to_fail})
 
 
 with DAG(
-    dag_id="example_wo_logging",
+    dag_id="example_without_logging",
     schedule_interval=None,
     start_date=datetime(2022, 1, 1),
     dagrun_timeout=timedelta(minutes=16),
     max_active_runs=2,
     concurrency=2,
-    tags=["wo_logging"],
+    tags=["logging"],
     catchup=False,
 ) as dag:
     sync_w_error = AwsLambdaInvokeFunctionOperator(
@@ -32,6 +32,35 @@ with DAG(
     )
 
     async_w_error = AwsLambdaInvokeFunctionOperator(
+        task_id="async_w_error",
+        function_name=LAMBDA_FUNCTION_NAME,
+        invocation_type="Event",
+        payload=_set_payload(),
+        aws_conn_id=None,
+    )
+
+    [sync_w_error, async_w_error]
+
+
+with DAG(
+    dag_id="example_with_logging",
+    schedule_interval=None,
+    start_date=datetime(2022, 1, 1),
+    dagrun_timeout=timedelta(minutes=16),
+    max_active_runs=2,
+    concurrency=2,
+    tags=["logging"],
+    catchup=False,
+) as dag:
+    sync_w_error = CustomLambdaFunctionOperator(
+        task_id="sync_w_error",
+        function_name=LAMBDA_FUNCTION_NAME,
+        invocation_type="RequestResponse",
+        payload=_set_payload(),
+        aws_conn_id=None,
+    )
+
+    async_w_error = CustomLambdaFunctionOperator(
         task_id="async_w_error",
         function_name=LAMBDA_FUNCTION_NAME,
         invocation_type="Event",
