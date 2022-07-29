6ce0d025ee1340fb9b9f7826d77d258498c38d26
Jaehyeon Kim <dottami@gmail.com>
2022-07-30 07:35:11 +1000

    refactor methods

--- airflow/dags/lambda_operator.py
+++ airflow/dags/lambda_operator.py
@@ -7,6 +7,7 @@ from uuid import uuid4
 import boto3
 from airflow.providers.amazon.aws.hooks.lambda_function import LambdaHook
 from airflow.providers.amazon.aws.operators.aws_lambda import AwsLambdaInvokeFunctionOperator
+from numpy import double
 
 if TYPE_CHECKING:
     from airflow.utils.context import Context
@@ -44,7 +45,8 @@ class CustomLambdaFunctionOperator(AwsLambdaInvokeFunctionOperator):
         @functools.wraps(func)
         def wrapper_decorator(self, *args, **kwargs):
             payload = func(self, *args, **kwargs)
-            self._process_log_events()
+            function_timeout = self._get_function_timeout()
+            self._process_log_events(function_timeout)
             return payload
 
         return wrapper_decorator
@@ -53,32 +55,30 @@ class CustomLambdaFunctionOperator(AwsLambdaInvokeFunctionOperator):
     def execute(self, context: "Context"):
         return super().execute(context)
 
-    def _process_log_events(self):
-        paginator = boto3.client("logs").get_paginator("filter_log_events")
-        timeout = self._get_function_timeout()
+    def _get_function_timeout(self):
+        lambda_client = boto3.client("lambda")
+        resp = lambda_client.get_function_configuration(FunctionName=self.function_name)
+        return resp["Timeout"]
+
+    def _process_log_events(self, function_timeout: int):
         status, start_time = None, 0
-        for _ in range(timeout):
-            response_iterator = paginator.paginate(
-                logGroupName=f"/aws/lambda/{self.function_name}",
-                filterPattern=f'"correlation_id" "{self.correlation_id}"',
-                startTime=start_time + 1,
-                # PaginationConfig={"MaxItems": 3},
-            )
-            status, start_time = self._parse_log_events(response_iterator)
+        for _ in range(function_timeout):
+            status, start_time = self._parse_log_events(start_time)
             if status == "succeeded":
                 break
             time.sleep(1)
         if status != "succeeded":
             raise RuntimeError("Lambda function end message not found after function timeout")
 
-    def _get_function_timeout(self):
-        lambda_client = boto3.client("lambda")
-        resp = lambda_client.get_function_configuration(FunctionName=self.function_name)
-        return resp["Timeout"]
-
-    def _parse_log_events(self, response_iterator: Any):
+    def _parse_log_events(self, start_time: int):
+        paginator = boto3.client("logs").get_paginator("filter_log_events")
         status = None
-        timestamp = 0
+        response_iterator = paginator.paginate(
+            logGroupName=f"/aws/lambda/{self.function_name}",
+            filterPattern=f'"correlation_id" "{self.correlation_id}"',
+            startTime=start_time + 1,
+            # PaginationConfig={"MaxItems": 3},
+        )
         for page in response_iterator:
             for event in page["events"]:
                 timestamp = event["timestamp"]
