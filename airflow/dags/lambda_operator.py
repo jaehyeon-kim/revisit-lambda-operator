14add3296dbc1f75d9f61a884a4211759fb9e9b4
Jaehyeon Kim <dottami@gmail.com>
2022-07-30 07:00:18 +1000

    update execute method with decorator

--- airflow/dags/lambda_operator.py
+++ airflow/dags/lambda_operator.py
@@ -1,6 +1,6 @@
-import os
 import time
 import json
+import functools
 from typing import TYPE_CHECKING, Optional, Any
 from uuid import uuid4
 
@@ -38,47 +38,23 @@ class CustomLambdaFunctionOperator(AwsLambdaInvokeFunctionOperator):
         )
         self.correlation_id = correlation_id
 
-    def execute(self, context: "Context"):
-        hook = LambdaHook(aws_conn_id=self.aws_conn_id)
-        success_status_codes = [200, 202, 204]
-        self.log.info(
-            "Invoking AWS Lambda function: %s with payload: %s", self.function_name, self.payload
-        )
-        response = hook.invoke_lambda(
-            function_name=self.function_name,
-            invocation_type=self.invocation_type,
-            log_type=self.log_type,
-            client_context=self.client_context,
-            payload=self.payload,
-            qualifier=self.qualifier,
-        )
-        self.log.info("Lambda response metadata: %r", response.get("ResponseMetadata"))
-        if response.get("StatusCode") not in success_status_codes:
-            raise ValueError(
-                "Lambda function did not execute", json.dumps(response.get("ResponseMetadata"))
-            )
-        payload_stream = response.get("Payload")
-        payload = payload_stream.read().decode()
-        if "FunctionError" in response:
-            raise ValueError(
-                "Lambda function execution resulted in error",
-                {"ResponseMetadata": response.get("ResponseMetadata"), "Payload": payload},
-            )
-        self.log.info("Lambda function invocation succeeded: %r", response.get("ResponseMetadata"))
-        self._process_log_events()
-        return payload
+    def _log_processor(func):
+        @functools.wraps(func)
+        def wrapper_decorator(self, *args, **kwargs):
+            payload = func(self, *args, **kwargs)
+            self._process_log_events()
+            return payload
 
-    def _get_function_timeout(self):
-        lambda_client = boto3.client("lambda")
-        resp = lambda_client.get_function_configuration(FunctionName=self.function_name)
-        return resp["Timeout"]
+        return wrapper_decorator
+
+    @_log_processor()
+    def execute(self, context: "Context"):
+        return super().execute(context)
 
     def _process_log_events(self):
         paginator = boto3.client("logs").get_paginator("filter_log_events")
-
         timeout = self._get_function_timeout()
-        status = None
-        start_time = 0
+        status, start_time = None, 0
         for _ in range(timeout):
             response_iterator = paginator.paginate(
                 logGroupName=f"/aws/lambda/{self.function_name}",
@@ -86,14 +62,19 @@ class CustomLambdaFunctionOperator(AwsLambdaInvokeFunctionOperator):
                 startTime=start_time + 1,
                 # PaginationConfig={"MaxItems": 3},
             )
-            status, start_time = self._parse_events(response_iterator)
+            status, start_time = self._parse_log_events(response_iterator)
             if status == "succeeded":
                 break
             time.sleep(1)
         if status != "succeeded":
             raise RuntimeError("Lambda function end message not found after function timeout")
 
-    def _parse_events(self, response_iterator: Any):
+    def _get_function_timeout(self):
+        lambda_client = boto3.client("lambda")
+        resp = lambda_client.get_function_configuration(FunctionName=self.function_name)
+        return resp["Timeout"]
+
+    def _parse_log_events(self, response_iterator: Any):
         status = None
         timestamp = 0
         for page in response_iterator:
