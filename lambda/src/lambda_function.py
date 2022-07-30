b45e24c7d43f27dad1758dcddb59af4e0f0838ce
Jaehyeon Kim <dottami@gmail.com>
2022-07-30 21:18:30 +1000

    update log record order

--- lambda/src/lambda_function.py
+++ lambda/src/lambda_function.py
@@ -3,7 +3,7 @@ from aws_lambda_powertools import Logger
 from aws_lambda_powertools.utilities.typing import LambdaContext
 from aws_lambda_powertools.middleware_factory import lambda_handler_decorator
 
-logger = Logger()
+logger = Logger(log_record_order=["correlation_id", "level", "message", "location"])
 
 
 @lambda_handler_decorator
